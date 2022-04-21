375e48f2ee88077df569bf7a9b6adb43a097e84d
gabbrieu <gabrielhmendes@hotmail.com>
2022-04-20 22:07:09 -0300

    initial configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  const configService = app.get(ConfigService);
+  const PORT = configService.get('PORT');
+  await app.listen(PORT, '0.0.0.0');
 }
 bootstrap();
