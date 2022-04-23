28662d677f75325ca93011ed10131f5eb209cee0
gabbrieu <gabrielhmendes@hotmail.com>
2022-04-23 13:49:10 -0300

    wip: adding group logic to getAllRegistersByDate method

--- src/modules/cases/cases.service.ts
+++ src/modules/cases/cases.service.ts
@@ -10,6 +10,35 @@ export class CasesService {
   ) {}
 
   async getAllRegistersByDate(date: string) {
-    const result = await this.repository.find({ date });
+    const result: Cases[] = await this.repository.find({ date });
+    const groupedResultByLocation = this.groupByProperty(result, 'variant');
+    const test = Object.entries(groupedResultByLocation).map(
+      ([variant, data]) => ({
+        variant,
+        data,
+      }),
+    );
+
+    test.forEach((t) => {
+      const testGroup = this.groupByProperty(t.data, 'location');
+      t.data = Object.entries(testGroup).map(([location, registers]) => ({
+        location,
+        registers,
+      })) as any;
+    });
+
+    return test;
+  }
+
+  private groupByProperty<T extends Record<string, any>, K extends keyof T>(
+    array: T[],
+    key: K | { (obj: T): string },
+  ): Record<string, T[]> {
+    const keyFn = key instanceof Function ? key : (obj: T) => obj[key];
+    return array.reduce((objectsByKeyValue, obj) => {
+      const value = keyFn(obj);
+      objectsByKeyValue[value] = (objectsByKeyValue[value] || []).concat(obj);
+      return objectsByKeyValue;
+    }, {} as Record<string, T[]>);
   }
 }
